1f21104970101878e82e206a051a89b60c01c6bf
PiDiaries <iainjohnston79@gmail.com>
2017-10-01 17:15:15 +0100

    serial flash lights

--- SerialLightsRespond/SerialLightsRespond.ino
+++ SerialLightsRespond/SerialLightsRespond.ino
@@ -9,14 +9,14 @@
  int led2 = 6;
  int motion = 3;
  int wait = 500; //500ms = 1/2s
- 
+
  void setup(){
   //put your setup code here, to run once:
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(motion,INPUT);
   Serial.begin(9600);
- }
+}
 
 
 void loop() {
@@ -33,10 +33,9 @@ void flash(int repetitions) {
   for (int i = 0; i < repetitions; i ++){
     digitalWrite(led1,HIGH);
     digitalWrite(led2,HIGH);
-    delay(wait)
+    delay(wait);
     digitalWrite(led1,LOW);
     digitalWrite(led2,LOW);
     delay(wait);
   }
 }
-
